a9344cadc8d2e3f5571edbe74829bce1bc365586
edilsonoliveirama <edilson.oliveira.ma@gmail.com>
2021-05-02 19:27:15 -0300

    Primeira parte de scripts feitos em Python através do Curso DIO

---
+++ aula2.py
@@ -0,0 +1,11 @@
+print("Bem vindo ao App Teste123")
+
+nome1 = input("Digite seu nome:")
+idade1 = int(input("Informe sua idade:"))
+
+nome2 = input("Digite seu nome:")
+idade2 = int(input("Informe sua idade:"))
+if idade1 > idade2 :
+    print (nome1 + " Você é mais velho(a) que" +nome2)
+else:
+    print(nome2 + "Você é mais velho(a) que" +nome1)
